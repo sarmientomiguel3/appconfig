cc92e690c296b6c0836d98dc4c5803c812da8727
sarmientomiguel3 <sarmientomiguel3@gmail.com>
2023-05-11 19:23:00 -0500

    appconfig

---
+++ src/test/java/com/aforo255/appconfig/AppconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aforo255.appconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
